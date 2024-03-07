bd6396cf93b1dda21c6121d4aa37d369da6f7de9
Zhaozhuo GAN <41332305+SiuKam@users.noreply.github.com>
2024-03-07 16:09:40 +0800

    Update abaqusRun.py

--- abaqusRun.py
+++ abaqusRun.py
@@ -30,13 +30,19 @@ for fileName in fileList:
     statFile = open(staName, 'r')
     lines = statFile.readlines()
     state = lines[-1].split(' ')[-1]
+    if state == 'SUCCESSFULLY':
+        res = 'finished'
+    elif state == 'COMPLETED':
+        res = 'NOT finished'
+    else:
+        res = 'state unknow'
     # 选取文件后两行，按需选取
     mailContent = '<p>' + lines[-3] + '<br>' + lines[-1] + '</p>'
     # 收发件信息
     message = Mail(
         from_email='from@email.com',
         to_emails='to@email.com',
-        subject=fileName + ' ' + state,
+        subject=str.join(fileName, ' ', res),
         html_content=mailContent)
     try:
         # 替换SendGrid API Key
